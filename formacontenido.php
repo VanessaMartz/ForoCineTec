cbdde0495526e763588c1d5120f348fb10f784c9
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 11:02:10 -0600

    Create formacontenido.php

---
+++ formacontenido.php
@@ -0,0 +1,25 @@
+<!--Archivo formacontenido principal reglas del foro-->
+<header class="inicio">
+    <img src="img/ticket1.png">
+    <span>CineTec</span>
+    <h3>Foro de Series y Películas</h3>
+    <a href="#principal"">
+      <img id="scroll-inicio" src="img/flecha1.png">
+  </a>
+  </header>
+
+  <section id="principal">
+    <h1>Reglas del foro</h1>
+    <h2>CineTec</h2>
+    <br>
+    <section id="lista">
+      <p>Estas reglas generales son importantes para que se cree un ambiente propicio con discusiones<br>
+        amenas de respeto y con contenido productivo, que faciliten el análisis crítico de diversas series y películas.</p>
+      <p>1.- Respetar las opiniones y ser respetuoso en los comentarios de los demás usuarios.</p>
+      <p>2.- Utilizar un vocabulario apropiado y evitar uso de lenguaje soez. Evitar expresiones<br>que puedan resultar ofensivas en temas de religión, género, raza, política, etc.</p>
+      <p>3.- Escribe en forma clara, lógica y concisa. Antes de publicar tu comentario asegúrate<br>de corregir errores gramaticales y ortográficos.</p>
+      <p>4.- En este foro está prohibido el uso de SPAM (Publicidad no deseada). No está permitida<br>la publicidad comercial o la promoción de otros sitios personales con fines de lucro.</p>
+      <p>5.- No está permitido el posteo de “Links” (enlaces) o descargas directas o sitios “ftp:” de<br>cualquier material con derechos de autor (libros, películas, música, software, etc.).</p>
+    </section>
+  </section>
+  
