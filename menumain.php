5a3147617c1a917399f2a000782e0afee1f5932b
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 12:56:40 -0600

    Create menumain.php

---
+++ menumain.php
@@ -0,0 +1,12 @@
+<!--MENU DE NAVEGACIÓN PRINCIPAL-->
+<nav class="navbar index-nav fixed-top">
+      <div class="container">
+      <a id="home" href="index.php">
+        <img src="img/ticket-inicio.png">
+      </a>
+      <a class="nav-link" href="nosotros/">Nosotros</a>
+      <a class="nav-link" href="login/">Iniciar sesión</a>
+      <a class="nav-link" href="registro/">Registrarse</a>
+  </div>
+  </nav>
+  <!--AGREGAR LINKS ENTRE LAS PESTAÑAS DE NOSOTROS,...,-->
