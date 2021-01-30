ee958ee1a0631045ff5febe3c04c6495ad49870c
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 12:57:08 -0600

    Create menu.php

---
+++ nosotros/menu.php
@@ -0,0 +1,11 @@
+<nav class="navbar index-nav fixed-top">
+
+     <a id="home" href="../index.php"> 
+        <img src="../img/ticket-inicio.png">
+      </a>
+      <div class="container">
+      <a class="nav-link" href="../nosotros">Nosotros</a>
+      <a class="nav-link" href="../login" >Iniciar sesión</a> 
+      <a class="nav-link" href="../registro">Registrarse</a>
+  </div>
+  </nav>
