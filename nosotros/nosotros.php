4d8ad1829e5c9a4102338f6f80f8396071652dca
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 12:57:13 -0600

    Create nosotros.php

---
+++ nosotros/nosotros.php
@@ -0,0 +1,46 @@
+<!--Pestaña nosotros-->
+  <main class="fondonosotros">
+    <div class="nosotros">
+    </div>
+    <div class="texto">
+        <span>
+            Nuestro proyecto es la creación de un foro básico para comentar
+            acerca de series o películas según las categorías de género comunes, 
+            en nuestro caso ofrecemos 10 categorías en las cuales se pueden crear
+            hilos para interactuar sobre temas de estos géneros. Con esta 
+            interacción, los usuarios de la página obtendrán una perspectiva 
+            de los comentarios de las demás personas, retroalimentando cada vez
+            más a la pagina con nuevas entradas de temas sobre series o películas.
+        </span>
+    </div>
+    <div>
+      <img class="iconocine" src="../img/icono1cine.png" id="icono1cine">
+    </div>
+    <div class="frase">
+      <span>
+          Si puede ser <strong>escrito</strong> o <strong>pensado</strong>,
+          puede ser filmado.
+          <p>- Stanley Kubrick</p>
+      </span>
+    </div>
+    <div class="texto">
+        <span>
+            La funcionalidad general de la página consiste en crear temas de las categorías:
+            Acción, Animación, Comedia, Ciencia Ficción, Crimen, Drama, Misterio, Romance, Suspenso
+            o Terror y postear temas y comentarios sobre estos. Según el nivel de usuario, se 
+            podrán ejecutar determinadas acciones.
+            
+        </span>
+    </div>
+    <div class="texto">
+      <h1>Equipo desarrollador</h1>
+    </div>
+    <div class="integrantesfoto">
+      <p><img src="../img/Equipo/vane.jpeg" alt="Vanessa Martínez Díaz"> Vanessa Martínez Díaz</p>
+      <p><img src="../img/Equipo/raquel.jpeg" alt="Raquel Rodríguez Virgen"> Raquel Rodríguez Virgen</p>
+      <p><img src="../img/Equipo/aldo.jpeg" alt="Aldo Felipe Eguan Marrufo"> Aldo Felipe Eguan Marrufo</p>
+      <p><img src="../img/Equipo/gabino.jpeg" alt="Gabino de Jesus Gómez Ramos"> Gabino de Jesus Gómez Ramos</p>
+      <p><img src="../img/Equipo/eduardo.jpeg" alt="Eduardo Antonio Hernandez Cabiedes"> Eduardo Antonio Hernandez Cabiedes</p>
+      <p><img src="../img/Equipo/jose.jpeg" alt="Jose Alonso Falfan Rosete"> Jose Alonso Falfan Rosete</p>
+    </div>
+  </main>
