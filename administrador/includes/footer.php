45ee39f1495983e627577b65a4f8bc2095bfc54a
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 10:56:13 -0600

    Create footer.php

---
+++ administrador/includes/footer.php
@@ -0,0 +1,11 @@
+
+</body>
+<script src="../js/jquery.min.js"></script>
+<script src="../bt4/js/bootstrap.min.js"></script>
+<script src= "../dt/datatables.js"></script>
+<script type= "text/javascript" src="../js/function.js"></script>
+<script>$(document).ready( function () {
+    $('#mitabla').DataTable();
+} );</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"> 
+</html>
