c612176773b5f0deaf8f328ab68115ace9f723b4
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 11:05:12 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php /*le indica al navegador que vienen sentencias del lado del backend*/
+     require_once "includes/header.php";
+     require_once "menumain.php";
+     require_once "formacontenido.php";
+     require_once "includes/footer.php";
+     require_once "formafooter.php";
+  
+
+?>
