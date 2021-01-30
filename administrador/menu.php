a6b97b8bc8db3d4da7281f1012da36d91734b5fa
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 10:56:39 -0600

    Create menu.php

---
+++ administrador/menu.php
@@ -0,0 +1,12 @@
+
+<nav class="navbar index-nav fixed-top">
+      <div class="container">
+      <a id="home" href="../index.php">
+        <img src="../img/ticket-inicio.png">
+      </a>
+      <a class="nav-link" href="index.php">Hilos</a>
+      <a class="nav-link" href="peticiones.php">Registros por confirmar</a>
+      <a class="nav-link" href="accesos.php">Accesos</a>
+      <a class="nav-link" href="logout.php">Cerrar sesión</a> 
+  </div>
+ </nav>
