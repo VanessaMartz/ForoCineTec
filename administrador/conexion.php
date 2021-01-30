a68a62f39da533fc0ab117694a3e97f45c221906
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 13:13:34 -0600

    Update conexion.php

--- administrador/conexion.php
+++ administrador/conexion.php
@@ -1,8 +1,8 @@
 <?php
 $dbhost = 'localhost';
-$dbuser = 'u117281852_w1304';
-$dbpass = 'ProgramacionWeb62$';
-$dbdata = 'u117281852_w1304';
+$dbuser = 'root';
+$dbpass = '';
+$dbdata = 'foro';
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbdata);
 if($conn->connect_error){
