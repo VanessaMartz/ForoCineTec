2b85da05dbe130645049bc3e447ce347f4bc0224
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 10:56:24 -0600

    Create header.php

---
+++ administrador/includes/header.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Foro </title> 
+        <link rel = "stylesheet" href = "../bt4/css/bootstrap.min.css">
+        <link rel = "stylesheet" href = "../dt/datatables.css">
+        <link rel = "stylesheet" href = "../faw/css/font-awesome.css">
+        <link rel = "stylesheet" href = "../css/dcc.css"> 
+        <link rel="stylesheet" href="../css/foro.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/formalogin.css">
+    </head>
+    <body >
+
