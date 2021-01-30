70a0db75704c180f20b9ecb060d6f30996a9b683
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 10:56:04 -0600

    Create formafooter.php

---
+++ administrador/formafooter.php
@@ -0,0 +1,29 @@
+<!--Archivo footer-->
+<footer>
+    <section>
+        <div>
+          <p class="texto1">Programación Web</p>
+          <p class="texto2">13 - 14 Hrs</p>
+          <p class="texto2">Profesor: Coss Camilo Delio</p>
+          </div>
+          <div>
+          <p class="texto1">Integrantes:</p>
+          <p>- Vanessa Martínez Díaz</p>
+          <p>- Raquel Rodríguez Virgen</p>
+          <p>- Aldo Felipe Eguan Marrufo</p>
+          <p>- Gabino de Jesus Gómez Ramos</p>
+          <p>- Eduardo Antonio Hernandez Cabiedes</p>
+          <p>- Jose Alonso Falfan Rosete</p>
+        </div>
+  </section>
+    <section>
+        <div>
+          <img class="logoitver" src="../img/tecbn.png"
+            alt="logoitver">
+        </div>
+        <div>
+          <img class="logotecnm" src="../img/logotecnm.png"
+          alt="logotecnm">
+        </div>
+  </section>
+  </footer>
