cdf5e3efe4ec4028d77bce71f416719faa3d12cd
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 11:05:03 -0600

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foro CineTec</title>
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../bt4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../dt/datatables.css">
+    <link rel="stylesheet" href="../faw/css/font-awesome.css">
+    <link rel="stylesheet" href="../css/foro.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/formalogin.css">
+</head>
+<body>
+    
+ 
